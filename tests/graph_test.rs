268509d3278bdd71e5ab53eac4fa9bddd396d5ef
varunvd <varun.vd51@gmail.com>
2021-05-23 19:54:04 +0530

    Added a test case to create and delete a graph database with r2d2 connection pool.

--- tests/graph_test.rs
+++ tests/graph_test.rs
@@ -2,6 +2,7 @@ mod common;
 
 use redisgraph::Graph;
 use serial_test::serial;
+use r2d2_redis::{r2d2, redis, RedisConnectionManager};
 
 use common::*;
 
@@ -11,5 +12,18 @@ fn test_open_delete() {
     let mut conn = get_connection();
 
     let graph = Graph::open(&mut conn, "test_open_delete_graph".to_string()).unwrap();
+    graph.delete().unwrap();
+}
+
+#[test]
+#[serial]
+fn test_open_delete_r2d2_connection_pool() {
+    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
+    let pool = r2d2::Pool::builder()
+        .max_size(20)
+        .build(manager)
+        .unwrap();
+    let  mut conn = pool.get().unwrap();
+    let graph = Graph::open(&mut conn, "test_open_delete_graph_with_connection_pool".to_string()).unwrap();
     graph.delete().unwrap();
 }
