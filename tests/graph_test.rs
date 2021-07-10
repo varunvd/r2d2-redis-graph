62cbd8e3e68408fb0f865a1d4ca69c7c47e067a1
varunvd <varun.vd51@gmail.com>
2021-07-10 20:58:01 +0530

    Changed existing connection to pooled connection for all test cases

--- tests/graph_test.rs
+++ tests/graph_test.rs
@@ -15,15 +15,3 @@ fn test_open_delete() {
     graph.delete().unwrap();
 }
 
-#[test]
-#[serial]
-fn test_open_delete_r2d2_connection_pool() {
-    let manager = RedisConnectionManager::new("redis://localhost").unwrap();
-    let pool = r2d2::Pool::builder()
-        .max_size(20)
-        .build(manager)
-        .unwrap();
-    let  mut conn = pool.get().unwrap();
-    let graph = Graph::open(&mut conn, "test_open_delete_graph_with_connection_pool".to_string()).unwrap();
-    graph.delete().unwrap();
-}
